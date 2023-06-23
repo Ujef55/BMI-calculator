4589a7c991e2d2805a2c5bea614c4d46daa0b1b4
Ujef55 <ujef.kelawala123@gmail.com>
2023-06-23 15:10:41 +0530

    Minor changes

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const form = document.querySelector('form');
 
 form.addEventListener('submit', function(e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    const height = parseFloat(document.querySelector('#height').value);
-    const weight = parseFloat(document.querySelector('#weight').value);
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
     const result = document.querySelector('#results');
 
-    if (isNaN(height) || height <= 0 || height === '') {
-        result.innerHTML = 'The Information you provided is not correct!';
-    } else if (isNaN(weight) || weight <= 0 || weight === '') {
-        result.innerHTML = 'The Information you provided is not correct!';
+    if ((height || weight) === '' || (height || weight) < 0 || isNaN(height || weight)) {
+        result.innerHTML = `Provided Information is not correct!`;
     } else {
         const bmi = (weight / ((height * height)/10000)).toFixed(2);
         if (bmi < 18.6) {
@@ -23,4 +21,29 @@ form.addEventListener('submit', function(e) {
             result.innerHTML = `<span>${bmi}</span>`;
         }
     }
-});
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
